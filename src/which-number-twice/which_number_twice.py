97eb1235b30573512bd59b3f25c9e4b02d2e5250
Sam Hage <sam@sparkfund.co>
2015-10-27 19:49:34 -0400

    which number twice in javascript

---
+++ src/which-number-twice/which_number_twice.py
@@ -0,0 +1,6 @@
+def which_number_twice( arr ):
+	"""
+	**********************************************************************************************************************
+	I have an array where every number in the range 1..n appears once except for one number which appears twice.
+	Write a function for finding the number that appears twice.
+	"""
