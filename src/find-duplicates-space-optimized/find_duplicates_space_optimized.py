b19d1830db15b168778ab549e74e36769b98d0e1
Sam Hage <sam@sparkfund.co>
2015-10-27 21:06:29 -0400

    find duplicates in javascript

---
+++ src/find-duplicates-space-optimized/find_duplicates_space_optimized.py
@@ -0,0 +1,16 @@
+def find_duplicates( arr ):
+	"""
+	**********************************************************************************************************************
+	We have an array of integers, where:
+	
+	The integers are in the range 1..n
+	The array has a length of n+1
+	It follows that our array has at least one integer which appears at least twice. But it may have 
+	several duplicates, and each duplicate may appear more than twice.
+	
+	Write a function which finds any integer that appears more than once in our array.
+	
+	We're going to run this function on our new, super-hip Macbook Pro With Retina Display™. Thing is, 
+	the damn thing came with the RAM soldered right to the motherboard, so we can't upgrade our RAM.
+	So we need to optimize for space!
+	"""
